666b0bc92faff1fe82004124d1091e6c42edb687
woodelin-florveus <woodelinflorveus@gmail.com>
2021-02-22 15:46:07 -0500

    update learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,25 @@
 // Code Keypad Component Here
+
+function Keypad(){
+
+    function eventHandle(event){
+        event.preventDefault();
+        console.log('Entering Password...')
+    }
+
+    return (
+        <form onSubmit={eventHandle}> 
+            <label>
+                Password:
+            </label>
+                <input type="password" name="password" placeholder="Entering Password..." /> 
+        </form>            
+        )
+
+
+}
+
+
+
+
+export default Keypad;
